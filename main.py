b82bf37c79c5f2cc9734bcc23c89fa377a863edc
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-02-12 07:38:50 +0530

    created a paddle of size 5 x 1

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """In This project we will created pong game."""
 from turtle import Screen
 
+from paddle import Paddle
+
 # Screen setup
 screen = Screen()  # created an object
 screen.bgcolor("black")  # set a black color to a screen
 screen.title("Pong Game")  # set a title on gui
 screen.setup(width=800, height=600)  # set a height and width of a GUI
 
+paddle = Paddle()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,2 +1,14 @@
 """This class create a paddle and responsible of moving paddle when key is pressed"""
+from turtle import Turtle
 
+
+class Paddle(Turtle):
+    """This class will create a paddle """
+
+    def __init__(self):
+        super().__init__()
+        self.goto(x=350, y=0)
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.shape("square")
+        self.color("white")
+        self.penup()
